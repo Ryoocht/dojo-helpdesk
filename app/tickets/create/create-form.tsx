1673f6af1e2e1cb61171693dd1fbbcc151274f01
Ryochi Tanaka <ryo@ryochis-mbp.home>
2023-09-14 22:21:25 +1000

    added: client side component

---
+++ app/tickets/create/create-form.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const CreateForm = () => {
+  const router = useRouter();
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [priority, setPriority] = useState("low");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (
+    e: React.FormEvent<HTMLFormElement | HTMLButtonElement>
+  ) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    const ticket = {
+      title,
+      body,
+      priority,
+      user_email: "ryo@gmail.com",
+    };
+
+    const response = await fetch("http://localhost:4000/tickets", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        body: JSON.stringify(ticket),
+      },
+    });
+
+    if (response.status === 201) {
+      router.refresh();
+      router.push("/tickets");
+    }
+  };
+
+  return (
+    <form action="" className="w-1/2" onSubmit={handleSubmit}>
+      <label>
+        <span>Title:</span>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Body:</span>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>Priority:</span>
+        <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+          <option value="low">Low Priority</option>
+          <option value="medium">Medium Priority</option>
+          <option value="high">High Priority</option>
+        </select>
+      </label>
+      <button
+        className="btn-primary"
+        disabled={isLoading}
+        onSubmit={handleSubmit}
+      >
+        {isLoading && <span>Adding...</span>}
+        {!isLoading && <span>Add Ticket</span>}
+      </button>
+    </form>
+  );
+};
+
+export default CreateForm;
--- app/tickets/tickets-list.tsx
+++ app/tickets/tickets-list.tsx
@@ -21,7 +21,7 @@ const TicketsList = async () => {
         <div key={ticket.id} className="card my-5">
           <Link href={`/tickets/${ticket.id}`}>
             <h3>{ticket.title}</h3>
-            <p>{ticket.body.slice(0, 200)}...</p>
+            <p>{ticket.body?.slice(0, 200)}...</p>
             <div className={`pill ${ticket.priority}`}>
               {ticket.priority} priority
             </div>
